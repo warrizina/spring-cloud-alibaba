f43e11f24db76c4e1aff61c6a33e9273e3922806
大帅比 <1310052319@qq.com>
2021-08-06 18:54:32 +0800

    添加公共模块，支付模块待补充

---
+++ cloud-pay/src/main/java/com/datahunter/cn/pay/PayCloudApplication.java
@@ -0,0 +1,16 @@
+package com.datahunter.cn.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @date: 2021/8/6 16:56
+ * @author: wzd
+ */
+@SpringBootApplication
+public class PayCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayCloudApplication.class);
+    }
+}
